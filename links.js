28fafd3d832b739bc91768c60db8c93b813c0355
Lyall <lyall.yeet@gmail.com>
2024-04-28 06:05:40 +0100

    assets and links.js

---
+++ links.js
@@ -0,0 +1,25 @@
+const links = {
+    "Self hosted": [
+        { name: "Jellyfin", description: "Basically self-hosted Netflix", iconPath: "/assets/jellyfin.png", subdomain: "jellyfin" },
+        { name: "Jellyfin Vue", description: "Self-hosted Netflix but with a different UI", iconPath: "/assets/jellyfin-vue.png", subdomain: "jellyfin-vue" },
+        { name: "Jellyseerr", description: "Media requests for Jellyfin", iconPath: "/assets/jellyseerr.svg", subdomain: "jellyseerr" }
+    ],
+    "My things": [
+        { name: "GitHub", path: "https://github.com/Lyall-A", iconPath: "/assets/github.svg" },
+        { name: "Discord", path: "https://discord.com/users/492729974026141697", iconPath: "/assets/discord.svg" },
+        { name: "Steam", path: "https://steamcommunity.com/id/darwinpolio/", iconPath: "/assets/steam.svg" },
+        { name: "BeatLeader", path: "https://beatleader.xyz/u/76561198987282778", iconPath: "/assets/beatleader.webp" },
+        { name: "ScoreSaber", path: "https://scoresaber.com/u/76561198987282778", iconPath: "/assets/scoresaber.svg" },
+    ],
+    "Useful links": [
+        { name: "Ninite", description: "Install a bunch of programs at once", path: "https://ninite.com/" },
+        { name: "OpenCore Sanity Checker", description: "Check if your hackintosh config is good", path: "https://sc.ocutils.me/" },
+    ],
+    "LYALL ONLY!!!!!": [
+        { name: "Deluge", description: "TORRENTING!!!", iconPath: "/assets/deluge.png", subdomain: "deluge" },
+        { name: "Portainer", description: "DOCKER CONTAINERS!!!", iconPath: "/assets/portainer.svg", subdomain: "portainer" },
+        { name: "Radarr", description: "MOVIES!!!", iconPath: "/assets/radarr.svg", subdomain: "radarr" },
+        { name: "Sonarr", description: "SHOWS!!!", iconPath: "/assets/sonarr.svg", subdomain: "sonarr" },
+        { name: "Lidarr", description: "MUSIC!!!", iconPath: "/assets/lidarr.svg", subdomain: "lidarr" }
+    ]
+};
--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-const links = {
-    "Self hosted": [
-        { name: "Jellyfin", description: "Basically self-hosted Netflix", iconPath: "/assets/jellyfin.png", subdomain: "jellyfin" },
-        { name: "Jellyfin Vue", description: "Self-hosted Netflix but with a different UI", iconPath: "/assets/jellyfin-vue.png", subdomain: "jellyfin-vue" },
-        { name: "Jellyseerr", description: "Media requests for Jellyfin", iconPath: "/assets/jellyseerr.svg", subdomain: "jellyseerr" }
-    ],
-    "My things": [
-        { name: "GitHub", path: "https://github.com/Lyall-A" },
-        { name: "Discord", path: "https://discord.com/users/492729974026141697" },
-        { name: "BeatLeader", path: "https://beatleader.xyz/u/76561198987282778" },
-        { name: "ScoreSaber", path: "https://scoresaber.com/u/76561198987282778" },
-    ],
-    "Useful links": [
-        { name: "Ninite", description: "Install a bunch of programs at once", path: "https://ninite.com/" },
-        { name: "OpenCore Sanity Checker", description: "Check if your hackintosh config is good", path: "https://sc.ocutils.me/" },
-    ],
-    "LYALL ONLY!!!!!": [
-        { name: "Deluge", description: "TORRENTING!!!", iconPath: "/assets/deluge.png", subdomain: "deluge" },
-        { name: "Portainer", description: "DOCKER CONTAINERS!!!", iconPath: "/assets/portainer.svg", subdomain: "portainer" },
-        { name: "Radarr", description: "MOVIES!!!", iconPath: "/assets/radarr.svg", subdomain: "radarr" },
-        { name: "Sonarr", description: "SHOWS!!!", iconPath: "/assets/sonarr.svg", subdomain: "sonarr" },
-        { name: "Lidarr", description: "MUSIC!!!", iconPath: "/assets/lidarr.svg", subdomain: "lidarr" }
-    ]
-};
-
 const content = document.getElementById("content");
 Object.entries(links).forEach(([sectionName, section]) => {
     const linksContainer = document.createElement("div");
